d8de2a7956f717ad6debba24929d548737691f8c
choijaewoong <jwchoi0129@gmail.com>
2017-01-19 21:18:29 +0900

    질문 상세보기 기능 구현

--- src/main/java/com/example/web/QnaController.java
+++ src/main/java/com/example/web/QnaController.java
@@ -3,17 +3,22 @@ package com.example.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Question;
 import com.example.domain.QuestionRepository;
+import com.example.domain.User;
+import com.example.utils.HttpSessionUtils;
 
 @Controller
 public class QnaController {
@@ -30,13 +35,32 @@ public class QnaController {
 //		questions.add(question);
 //		log.info("users size : " + questions.size());
 //		log.debug("users : " + questions);
-
 		return "redirect:/";
 	}
 	
 	@GetMapping("/qna/form")
-	public String form() {	
+	public String form(Model model, HttpSession session) {
+		if(!HttpSessionUtils.isLoginUser(session)) {
+//			throw new IllegalStateException("로그인하지 않은 사용자...");
+			return "/user/login";
+		}
+		User loginUser = HttpSessionUtils.getUserFromSession(session);
+		model.addAttribute("user", loginUser);
 		return "/qna/form";
+	}
+	
+	@GetMapping("/qna/detail/{id}")
+	public String detail(@PathVariable long id, Model model) {
+		
+		Question question = questionRepository.findOne(id);
+		model.addAttribute("question", question);
+		return "/qna/show";
+		
+	}
+	
+	@GetMapping("/qna/show")
+	public String show() {
+		return "/qna/show";
 	}
 	
 	@GetMapping("/")
